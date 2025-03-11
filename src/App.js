6c7b908c639266edfc5f328ef4b5d0b3fcca5716
stephanieessis@gmail.com <stephanieessis@gmail.com>
2025-03-11 18:56:01 +0000

    fix:profile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: "Legue You",
+        bio: "Développeur Full Stack passionné par React",
+        imgSrc: "/assets/jeune.avif ",
+        profession: "Développeur Web",
+      },
+      show: false,
+      mountedTime: 0,
+    };
+    this.toggleShow = this.toggleShow.bind(this);
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState((prevState) => ({ mountedTime: prevState.mountedTime + 1 }));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  toggleShow() {
+    this.setState({ show: !this.state.show });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Profil de la Personne</h1>
+        <button onClick={this.toggleShow}>
+          {this.state.show ? "Masquer le profil" : "Afficher le profil"}
+        </button>
+        {this.state.show && (
+          <div className="profile">
+            <img src={this.state.person.imgSrc} alt={this.state.person.fullName} />
+            <h2>{this.state.person.fullName}</h2>
+            <p>{this.state.person.bio}</p>
+            <h3>{this.state.person.profession}</h3>
+        </div>
+        )}
+        <p>Temps écoulé depuis le montage : {this.state.mountedTime} secondes</p>
     </div>
-  );
+    );
+}
 }
 
 export default App;
